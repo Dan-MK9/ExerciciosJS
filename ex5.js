06e01189380c97bf1090f0837ac94bf0a2d55ce5
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-05-06 16:39:18 -0300

    feat: ex5 corrigido

--- ex5.js
+++ ex5.js
@@ -3,9 +3,9 @@
 //crescente. Exibindo uma mensagem indicando se os números estão em ordem crescente ou
 //não.
 
-let num1 = prompt ("Digite o primeiro número")
-let num2 = prompt ("Digite o segundo o número")
-let num3 = prompt ("Digite o terceiro número")
+let num1 = prompt("Digite o primeiro número")
+let num2 = prompt("Digite o segundo o número")
+let num3 = prompt("Digite o terceiro número")
 
 if (num1 < num2 && num2 < num3) {
     alert("Os números estão em ordem crescente")
--- ex7.js
+++ ex7.js
@@ -8,3 +8,22 @@
 // Valor total da compra acima de R$ 200,00: desconto de 20%.
 // Exiba o valor final da compra com o desconto aplicado.
 
+let valorTotal = parseFloat(prompt("Informe o valor total da compra: "))
+
+let desconto;
+
+if (valorTotal <= 100){
+    desconto = 0;
+    console.log("O valor de desconto foi :" + desconto);
+}
+else if(valorTotal <= 200){
+    desconto = 0.1;
+    console.log("O valor de desconto foi de: " + (desconto*100)+"%");
+}else{
+    desconto = 0.2;
+    console.log("O valor de desconto foi de " + (desconto*100+"%"))
+}
+
+let valorFinal = valorTotal - (valorTotal*desconto);
+
+console.log("Valor final da compra com o desconto: R$" + valorFinal.toFixed(2));
