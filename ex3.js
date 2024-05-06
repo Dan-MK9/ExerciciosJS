81cf34063c89fc3d5fcb663bc1a4b2528e5ab5b6
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-05-06 15:29:06 -0300

    feat: ex3 corrigido

--- ex3.js
+++ ex3.js
@@ -11,3 +11,26 @@ if(number & 1){
 } else {
     alert("par");
 }
+
+// O operador & é usado aqui como uma operação de bitwise AND 
+// para verificar se o último bit do número é 1, indicando que o número é ímpar.
+
+// A explicação em exemplo para este operador (bitwise AND):
+
+// 1
+// 00000001
+
+// 2
+// 00000010
+
+// 3
+// 00000011
+
+// 4
+// 00000100
+
+// 5
+// 00000101
+
+// 6
+// 00000110
