750ecfd20df192d671aa9efbea7bce45018ca4f0
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-05-07 16:55:50 -0300

    feat: ExLoop1

---
+++ loop/exemploRepeticao.js
@@ -0,0 +1,53 @@
+// for - Loop contado//
+
+// for(let contador = 0; contador < 5; contador++){
+//     prompt ("Digite seu nome: ")
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// while - Loop condicional//
+
+// let opcao = prompt("Digite uma opcao\nDigite 0 para sair:")
+
+// while (opcao != "0") {
+//     console.log("Repitiu")
+// }
+
+// console.log("Saiu")
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// do -while -> Loope condicional//
+
+// do {
+//     let valor = prompt("Digite um numero: ")
+//     switch (valor) {
+//         case "1":
+//             console.log("Voce digitou o valor 1")
+//             break;
+
+//         case "2":
+//             console.log("Voce digitou o valor 2")
+//             break;
+
+//         case "3":
+//             console.log("Voce digitou o valor 3")
+//             break; 
+            
+//         default:
+//             console.log("Valor invalido")
+//             break;
+//     }
+// } while (valor == "0");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ForEach(Para cada) - percorrer a lista //
+
+// let num = [1,2,3,4,5,6,7];
+
+// num.forEach(elemento => {
+//     console.log("O numero e\-: " + elemento)
+
+// });
