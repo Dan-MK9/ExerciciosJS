510c9bb733f527a48cf6f5f3cb7d905c35fc8bb7
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-05-08 16:53:21 -0300

    feat: exLoop10

---
+++ loop/exLoop10.js
@@ -0,0 +1,10 @@
+// 10. Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+while (true){
+    let num = prompt("Digite um numero: ")
+    if (num == "0"){
+        console.log ("numero 0 digitado")
+        break
+    }
+}
