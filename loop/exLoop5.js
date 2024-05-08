211e04900c1a33f4ecebffcdd38adfb30921d5cc
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-05-08 15:27:08 -0300

    feat: exLoop5

---
+++ loop/exLoop5.js
@@ -0,0 +1,14 @@
+// 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+
+let numA = 0
+let numB = 1
+let numC 
+
+for(let contador = 0; contador < 10; contador++){
+    console.log(numA)
+
+    numC = numA + numB
+    numA = numB
+    numB = numC
+
+}
