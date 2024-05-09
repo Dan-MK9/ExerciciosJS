454a5deed70ca23a54d4a420bc6f924d28d6c7a1
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-05-09 17:04:15 -0300

    feat: exLoop7 corrigido

---
+++ funcao/exemploFuncao.js
@@ -0,0 +1,21 @@
+// Chamanfo a função alert
+// alert("Hello World")
+
+// Chamando o método log ()
+// console.log("Olá Danilo")
+
+// // Declaração da função - Ensinando o computador
+//  function BemVindo(nome) {
+//      console.log("Bem Vindo " + nome);
+// }
+
+// Chamada da função - executa a função
+// BemVindo("Danilo")
+
+function somar(num1, num2){
+    return num1 + num2;
+}
+
+let resultado = somar(3, 7);
+
+console.log(resultado);
---
+++ funcao/exemploObjeto.js
@@ -0,0 +1,22 @@
+// // nome
+// let nome = "Danilo"
+
+// // apelido
+// let apelido = "Dan"
+
+let pessoa1 = {
+    nome: "Danilo",
+    apelido: "Dan"
+}
+
+let pessoa2 = {
+    nome: "Felipe",
+    apelido: "Bixona"
+}
+
+let produto = {
+    nomeProduto: "Shampoo",
+    marca: "CR7",
+    descricao: "Um homem, uma maquina",
+    preco: "29.98"
+}
