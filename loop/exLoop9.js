9f9567cabb6cfb9294a159831da601a90cef9255
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-05-09 14:50:15 -0300

    feat: exLoop9 corrigido

--- loop/exLoop9.js
+++ loop/exLoop9.js
@@ -1,7 +1,7 @@
 // 9. Faça um programa que receba um número do usuário e mostre
 // no console a contagem regressiva até 0.
 
-let num = prompt ("Digite um numero: ")
+let num = parseInt(prompt("Digite um numero: "))
 
 while (num >= 0) {
     console.log (num)
